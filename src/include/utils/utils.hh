bdb17717d794f3d4712098fb6a8db305dc825c06
Mathieu Nativel <mathieu.nativel@epita.fr>
2020-11-20 08:36:37 +0100

    utils: add ptrace string map and forgotten signalstr array

--- src/include/utils/utils.hh
+++ src/include/utils/utils.hh
@@ -1,5 +1,12 @@
 #pragma once
 
+#include <sys/ptrace.h>
+
+#include <map>
+#include <string>
+
+
 namespace utils {
     extern std::string signalstr[31];
+    extern std::string ptrace_request_str[];
 }
